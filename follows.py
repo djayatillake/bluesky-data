e7fe3905facdab4fe5c1d004084c45ecfb06d3d8
djayatillake <dave665@gmail.com>
2024-12-03 09:54:32 +0000

    refactor to allow pipelines to be used across files

--- follows.py
+++ follows.py
@@ -9,7 +9,7 @@ from utils import (
     pipeline,
 )
 
-table_name = "followers"
+table_name = "follows"
 
 pipeline.run(get_followers(actor).add_map(create_actor_field(actor)),
              table_name=table_name,
@@ -23,12 +23,16 @@ con.execute(sql)
 actors = con.fetchall()
 
 for index, actor in enumerate(actors):
-    pipeline.run(get_followers(actor[0]).add_map(create_actor_field(actor[0])),
+    pipeline.run(get_follows(actor[0]).add_map(create_actor_field(actor[0])),
                  table_name=table_name,
                  write_disposition="append",
                  )
-    pipeline.run(get_follows(actor[0]).add_map(create_actor_field(actor[0])),
-                 table_name="follows",
+    pipeline.run(get_followers(actor[0]).add_map(create_actor_field(actor[0])),
+                 table_name="followers",
                  write_disposition="append",
                  )
     print(actor[0], index)
+
+# TODO: it would be good to find who the followers of people I follow are also following
+# and add that data to the follows table, but it is likely to be a lot of data and a huge number of API calls
+# so we need to be probably take a cut of people who are followed a lot by the followers of my follows
